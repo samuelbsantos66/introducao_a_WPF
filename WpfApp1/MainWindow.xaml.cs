633e6da722452c69c39b90909eb485bdd8026750
Samuel Barbosa Santos <125471851+samuelbsantos66@users.noreply.github.com>
2024-08-29 08:47:16 -0300

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+            Contador.Content = count.ToString();
+
+        }
+
+        private void Cadastro_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w1 = new();
+            w1.Show();
+        }
+    }
+}
